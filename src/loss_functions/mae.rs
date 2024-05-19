3771e4bfb108d5e1cf7bd99163e132bb599b6a2e
Josh Friedson <business@joshfriedson.com>
2024-05-18 20:26:39 -0400

    mean absolute error implementation

---
+++ src/loss_functions/mae.rs
@@ -0,0 +1,32 @@
+use crate::loss_functions::loss_function::LossFunction;
+use ndarray::{ArrayD, ArrayViewD};
+use ndarray_rand::rand_distr::num_traits::abs;
+
+pub struct MAE {}
+
+impl MAE {
+    pub fn new() -> Self {
+        MAE {}
+    }
+}
+
+impl LossFunction for MAE {
+    fn forward(&self, predictions: &ArrayViewD<f32>, truths: &ArrayViewD<f32>) -> f32 {
+        let mut absolute_error = predictions - truths;
+        absolute_error.mapv_inplace(|x| abs(x));
+
+        absolute_error.mean().unwrap()
+    }
+
+    fn backward(&self, predictions: &ArrayViewD<f32>, truths: &ArrayViewD<f32>) -> ArrayD<f32> {
+        ArrayD::<f32>::from_shape_fn(predictions.raw_dim(), |i| {
+            if predictions[&i] > truths[&i] {
+                1.
+            } else if predictions[&i] < truths[&i] {
+                -1.
+            } else {
+                0.
+            }
+        })
+    }
+}
